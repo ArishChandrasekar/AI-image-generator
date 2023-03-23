2317e41eb4a84b1db242f613359a84d65b6d63e6
ArishChandrasekar <arishchandrasekar@gmail.com>
2023-03-23 21:36:15 +0530

    fourth

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -22,13 +22,16 @@ const CreatePost = () => {
         setGeneratingImg(true);
 
         //calling dalleroute from server
-        const response = await fetch("http://localhost:5000/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "https://dall-e-ah9l.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
 
         const data = await response.json();
 
@@ -52,13 +55,16 @@ const CreatePost = () => {
 
       try {
         //calling postroute from server
-        const response = await fetch("http://localhost:5000/api/v1/post", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ...form }),
-        });
+        const response = await fetch(
+          "https://dall-e-ah9l.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...form }),
+          }
+        );
         await response.json();
         navigate("/");
       } catch (error) {
